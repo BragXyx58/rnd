ca1c0a91554464e1ef1ceb79be18cde1bbdbb9ca
BragXyx58 <BragX_yx58@student.itstep.org>
2024-12-18 10:17:26 +0100

    added ListBox with idgnored values

--- rnd.py
+++ rnd.py
@@ -1,32 +1,54 @@
 from tkinter import *
-from tkinter import ttk,messagebox
+from tkinter import ttk, messagebox
 import random
+
 
 def generate_random_number():
     try:
-        min_val = int(combo_min.get())
-        max_val = int(combo_max.get())
+        min_val = int(num_min.get())
+        max_val = int(num_max.get())
         if min_val > max_val:
             messagebox.showerror("Ошибка", "'ОТ' не может быть больше 'ДО'")
         else:
             random_number = random.randint(min_val, max_val)
+            exceptions = [int(item) for item in list_exceptions.get(0, END)]
+            while random_number in exceptions:
+                random_number = random.randint(min_val, max_val)
+
             result.config(text=f"Случайное число: {random_number}")
     except ValueError:
-        result.config(text="Error")
+        result.config(text="Ошибка ввода")
+
+
+def add_exception():
+    try:
+        exception = int(entry_exception.get())
+        list_exceptions.insert(END, exception)
+        entry_exception.delete(0, END)
+    except ValueError:
+        messagebox.showerror("Ошибка", "Введите корректное число")
+
 
 root = Tk()
-root.geometry("300x300")
+root.geometry("300x400")
 root.title("Генератор случайных чисел")
 Label(root, text="От:").pack()
-combo_min = ttk.Combobox(root, values=list(range(0, 101)))
-combo_min.pack()
-combo_min.set("0")
+num_min = ttk.Combobox(root, values=list(range(0, 101)))
+num_min.pack()
+num_min.set("0")
 Label(root, text="До:").pack()
-combo_max = ttk.Combobox(root, values=list(range(0, 101)))
-combo_max.pack()
-combo_max.set("100")
+num_max = ttk.Combobox(root, values=list(range(0, 101)))
+num_max.pack()
+num_max.set("100")
+Label(root, text="Исключения:").pack()
+entry_exception = Entry(root)
+entry_exception.pack()
+button_add_exception = Button(root, text="Добавить исключение", command=add_exception)
+button_add_exception.pack(pady=5)
+list_exceptions = Listbox(root)
+list_exceptions.pack(pady=5)
 button_generate = Button(root, text="Сгенерировать число", command=generate_random_number)
-button_generate.pack()
+button_generate.pack(pady=10)
 result = Label(root, text="Случайное число: ")
 result.pack()
 root.mainloop()
