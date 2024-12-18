7585b7ca80cd5b65a46fdea720b2a00b1c7eedad
BragXyx58 <BragX_yx58@student.itstep.org>
2024-12-18 09:37:23 +0100

    added comboboxes for range

--- rnd.py
+++ rnd.py
@@ -1,13 +1,30 @@
 from tkinter import *
+from tkinter import ttk
 import random
 
 def generate_random_number():
-    random_number = random.randint(1, 100)
-    result.config(text=f"Случайное число: {random_number}")
+    try:
+        min_val = int(combo_min.get())
+        max_val = int(combo_max.get())
+        if min_val > max_val:
+            result.config(text="Ошибка: 'ОТ' больше 'ДО'")
+        else:
+            random_number = random.randint(min_val, max_val)
+            result.config(text=f"Случайное число: {random_number}")
+    except ValueError:
+        result.config(text="Error")
 
 root = Tk()
-root.geometry("200x200")
+root.geometry("300x300")
 root.title("Генератор случайных чисел")
+Label(root, text="От:").pack()
+combo_min = ttk.Combobox(root, values=list(range(0, 101)))
+combo_min.pack()
+combo_min.set("0")
+Label(root, text="До:").pack()
+combo_max = ttk.Combobox(root, values=list(range(0, 101)))
+combo_max.pack()
+combo_max.set("100")
 button_generate = Button(root, text="Сгенерировать число", command=generate_random_number)
 button_generate.pack()
 result = Label(root, text="Случайное число: ")
